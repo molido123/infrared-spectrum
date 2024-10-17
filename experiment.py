c7023042cb462f8149e421fd44d50b1e231d34a3
huagenda <3391394571@qq.com>
2024-10-17 10:32:33 +0800

    添加保存模型

--- experiment.py
+++ experiment.py
@@ -27,79 +27,6 @@ def trainModel(args):
     # TODO
 
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
-    # for train_index, test_index in kf.split(trainData):
-    #     trainData, trainLabel, testData, testLabel = trainData[train_index], trainLabel[train_index], trainData[
-    #         test_index], trainLabel[test_index]
-    #     X_train, X_test, y_train, y_test = train_test_split(trainData, trainLabel, test_size=0.2, random_state=42)
-
-    #     trainDataset = Data.TensorDataset(X_train, y_train)
-    #     testDataset = Data.TensorDataset(X_test, y_test)
-    #     trainDataLoader = Data.DataLoader(dataset=trainDataset, batch_size=args.batch_size, shuffle=True)
-    #     testDataLoader = Data.DataLoader(dataset=testDataset, batch_size=args.batch_size, shuffle=True)
-
-    #     model = UnetRemake(tasks=args.tasks).to(device)
-    #     criterion = nn.L1Loss()
-    #     optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-    #     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=args.gamma)
-
-        
-    #     for epoch in range(args.epoch):
-    #         R_task = [0] * args.tasks
-    #         total_loss = 0
-    #         total_R = 0
-    #         count = 0
-            
-    #         model.train()
-    #         for i, (data, label) in enumerate(trainDataLoader):
-    #             weights, output = model(data, label)
-    #             loss = criterion(output, label)
-    #             weightedLoss = torch.mul(weights, loss)  # 这里应该使用 mul 而不是 dot
-    #             weightedLoss = torch.sum(weightedLoss)
-    #             optimizer.zero_grad()
-    #             weightedLoss.backward()
-    #             # 这里使用权重损失来更新，是因为要考虑到不同任务的权重
-
-    #             for id in range(args.tasks):
-    #                 R_task[id] = calcCorr(output[:, id], label[:, id])
-
-    #             model.weights.grad.data = torch.zeros_like(model.weights.grad.data)
-
-    #             if i == 0:
-    #                 initial_loss = loss
-                
-    #             if args.mode == 'grad_norm':
-    #                 W = model.get_last_shared_layer()
-    #                 norms = []
-    #                 for j in range(args.tasks):
-    #                     gLgW = torch.autograd.grad(loss[j], W, retain_graph=True)[0]
-    #                     norms.append(torch.norm(torch.mul(model.weights[j], gLgW)))
-    #                 norms = torch.stack(norms)
-                    
-    #                 if torch.cuda.is_available():
-    #                     loss_ratio = loss.data().cpu().numpy() / initial_loss.data().cpu().numpy()
-    #                 else:
-    #                     loss_ratio = loss.data.numpy() / initial_loss.data.numpy()
-    #                 inverse_train_rate = loss_ratio / np.mean(loss_ratio)
-                    
-    #                 mean_norm = torch.mean(norms).to(device).numpy()
-                    
-    #                 constant = torch.tensor(mean_norm * (inverse_train_rate ** args.alpha), requires_grad=True).to(device)
-    #                 grad_norm_loss = torch.sum(torch.abs(norms - constant)).requires_grad_(True)
-                    
-    #                 model.weights.grad = torch.autograd.grad(grad_norm_loss, model.weights)[0]                    
-            
-    #             optimizer.step()
-    #             total_loss += torch.sum(loss).item() * data.size(0)
-    #             total_R += R_task * data.size(0)
-    #             count += data.size(0)
-                
-    #             normalize_coeff = args.tasks / torch.sum(model.weights.data, dim=0)
-    #             model.weights.data = model.weights.data * normalize_coeff
-
-    #         scheduler.step()
-    #         print(f'Epoch: {epoch + 1}, Loss: {total_loss / count}, Corr: {total_R / count}')
-    #         for i in range(args.tasks):
-    #             print(f'Corr of task {i + 1} in Training: {R_task[i]}')
 
     # 初始化存储交叉验证过程中损失、相关系数和RMSE的列表
     loss_test = []
@@ -238,8 +165,61 @@ def trainModel(args):
     # 最终的交叉验证结果
     for j in range(args.tasks):
         print(f'{task_names[j]} - Cross-validation results: Avg Loss: {np.mean(loss_test[j]):.4f}, Avg R: {np.mean(r_test[j]):.4f}, Avg RMSE: {np.mean(rmse_test[j]):.4f}')
-        
-            
+
+    # --- 使用所有数据进行最终训练，并保存模型 ---
+    print("Training final model with all data...")
+
+    # 构建最终的 DataLoader 使用全部训练数据
+    fullDataset = Data.TensorDataset(trainData, trainLabel)
+    fullDataLoader = Data.DataLoader(dataset=fullDataset, batch_size=args.batch_size, shuffle=True)
+
+    mmoe = PLE(num_task=args.tasks).to(device)
+    optimizer = torch.optim.Adam(mmoe.parameters(), lr=args.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=args.gamma)
+
+    # 训练模型（使用所有数据）
+    for epoch in range(args.epoch):
+        mmoe.train()
+        total_loss = 0
+        R_task_train = [0] * args.tasks
+        count = 0
+        for idx, (b_x, b_y) in enumerate(fullDataLoader):
+            b_x, b_y = b_x.to(device), b_y.to(device)
+            b_x = torch.unsqueeze(b_x, dim=1)  # 增加一个通道维度
+
+            # 模型前向计算
+            predict = mmoe(b_x)
+
+            # 计算每个任务的损失
+            task_loss = []
+            for j in range(args.tasks):
+                a = b_y[:, j]
+                a = torch.unsqueeze(a, dim=1)
+                task_loss.append(criterion(a, predict[j]))
+            task_loss = torch.stack(task_loss)
+
+            # 计算加权损失
+            weighted_task_loss = torch.mul(mmoe.weights, task_loss)
+            loss = torch.sum(weighted_task_loss)
+
+            optimizer.zero_grad()
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+            # 修正后的 total_loss 计算逻辑
+            total_loss += torch.sum(task_loss).item() * b_x.size(0)
+            for j in range(args.tasks):
+                R_task_train[j] += calcCorr(predict[j], b_y[:, j]).item() * b_x.size(0)
+            count += b_x.size(0)
+
+        scheduler.step()
+        print(f'Epoch {epoch + 1} - Final Train Loss: {total_loss / count:.4f}')
+        for j in range(args.tasks):
+            print(f'{task_names[j]} Final R_train: {R_task_train[j] / count:.4f}')
+
+    # 保存模型到指定路径
+    torch.save(mmoe.state_dict(), './checkpoint/model_final.pth')
+    print("Final model saved as 'model_final.pth'.")
             
 
 
